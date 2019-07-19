6b72192b1cfe75f3c00b3740fd1226c4ad2fe054
661YCJ <1157982786@qq.com>
2019-07-19 17:35:47 +0800

    login-1

---
+++ lib/register_page.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class RegisterPage extends StatefulWidget{
+  @override
+  _RegisterState createState() =>_RegisterState();
+}
+
+class _RegisterState extends State<RegisterPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password,_password_confirm;
+  bool _isObscure = true;
+  Color _eyeColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+          key: _formKey,
+          child:ListView(
+            padding: EdgeInsets.symmetric(horizontal: 22.0),
+            children: <Widget>[
+              SizedBox(
+                height: kToolbarHeight,
+              ),
+              buildTitle(),
+              buildTitleLine(),
+              SizedBox(height: 40.0),
+              buildEmailTextField(),
+              SizedBox(height: 25.0),
+              buildPasswordTextField(context),
+              SizedBox(height: 25.0),
+              bulidConfirmTextField(context),
+              SizedBox(height: 60.0,),
+              buildRegisterButton(context),
+              SizedBox(height: 20.0),
+            ],
+          )
+      )
+
+    );
+  }
+
+  Padding buildTitle() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Register',
+        style: TextStyle(fontSize: 42.0),
+      ),
+    );
+  }
+
+  Padding buildTitleLine() {
+    return Padding(
+      padding: EdgeInsets.only(left: 12.0, top: 4.0),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          color: Colors.black,
+          width: 40.0,
+          height: 2.0,
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Emall Address',
+      ),
+      validator: (String value) {
+        var emailReg = RegExp(
+            r"[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
+        if (!emailReg.hasMatch(value)) {
+          return '请输入正确的邮箱地址';
+        }else{
+          return 'error';
+        }
+      },
+      onSaved: (String value) => _email = value,
+    );
+  }
+
+  TextFormField buildPasswordTextField(BuildContext context) {
+    return TextFormField(
+      onSaved: (String value) => _password = value,
+      obscureText: _isObscure,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return '请输入密码';
+        }else{
+          return 'error';
+        }
+      },
+      decoration: InputDecoration(
+          labelText: 'Password',
+          suffixIcon: IconButton(
+              icon: Icon(
+                Icons.remove_red_eye,
+                color: _eyeColor,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isObscure = !_isObscure;
+                  _eyeColor = _isObscure
+                      ? Colors.grey
+                      : Theme.of(context).iconTheme.color;
+                });
+              })),
+    );
+  }
+
+  Align buildRegisterButton(BuildContext context) {
+    return Align(
+      child: SizedBox(
+        height: 45.0,
+        width: 270.0,
+        child: RaisedButton(
+          child: Text(
+            'Register',
+            style: Theme.of(context).primaryTextTheme.headline,
+          ),
+          color: Colors.black,
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              ///只有输入的内容符合要求通过才会到达此处
+              _formKey.currentState.save();
+              //TODO 执行登录方法
+              print('email:$_email , assword:$_password');
+            }
+          },
+          shape: StadiumBorder(side: BorderSide()),
+        ),
+      ),
+    );
+  }
+
+  TextFormField bulidConfirmTextField(BuildContext context) {
+    return TextFormField(
+      onSaved: (String value) => _password_confirm = value,
+      obscureText: _isObscure,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return '请确认密码';
+        }else{
+          return '';
+        }
+      },
+      decoration: InputDecoration(
+          labelText: 'Confirm',
+          suffixIcon: IconButton(
+              icon: Icon(
+                Icons.remove_red_eye,
+                color: _eyeColor,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isObscure = !_isObscure;
+                  _eyeColor = _isObscure
+                      ? Colors.grey
+                      : Theme.of(context).iconTheme.color;
+                });
+              })),
+    );
+  }
+}
+
